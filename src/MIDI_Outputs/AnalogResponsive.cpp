a0a9472d38516ddae467e9ea983f02f083e7ab64
alf45tar <35426671+alf45tar@users.noreply.github.com>
2018-01-14 20:42:13 +0100

    Added AnalogResponsive class

---
+++ src/MIDI_Outputs/AnalogResponsive.cpp
@@ -0,0 +1,41 @@
+#include "Arduino.h"
+#include "AnalogResponsive.h"
+#include "MIDI_Controller.h"
+
+using namespace ExtIO;
+
+AnalogResponsive::AnalogResponsive(pin_t analogPin, uint8_t controllerNumber, uint8_t channel) // Constructor
+{
+  this->analogPin = analogPin;
+  this->controllerNumber = controllerNumber;
+  this->channel = channel;
+  this->respAnalog = new ResponsiveAnalogRead(analogPin, true);
+}
+
+AnalogResponsive::~AnalogResponsive() // Destructor
+{
+  delete respAnalog;
+}
+
+void AnalogResponsive::invert() // Invert the button state (send Note On event when released, Note Off when pressed)
+{
+  invertState = true;
+}
+
+void AnalogResponsive::refresh() // read the analog value, update the average, map it to a MIDI value, check if it changed since last time, if so, send Control Change message over MIDI
+{
+  unsigned int input = ExtIO::analogRead(analogPin); // read the raw analog input value
+  if (invertState) input = 1023-input;				 // invert the scale
+  uint16_t value = analogMap(input); 				 // apply the analogMap function to the value (identity function f(x) = x by default)
+  respAnalog->update(value); 						 // update the responsive analog average
+  if (respAnalog->hasChanged()) 				 	 // if the value changed since last time
+  {
+    value = respAnalog->getValue(); // get the responsive analog average value
+	MIDI_Controller.MIDI()->send(CC, channel + channelOffset * channelsPerBank, controllerNumber + addressOffset * channelsPerBank, value); // send a Control Change MIDI event
+  }
+}
+
+void AnalogResponsive::map(int (*fn)(int)) // change the function pointer for analogMap to a new function. It will be applied to the raw analog input value in Analog::refresh()
+{
+  analogMap = fn;
+}
---
+++ src/MIDI_Outputs/AnalogResponsive.h
@@ -0,0 +1,34 @@
+#ifndef AnalogResponsive_h_
+#define AnalogResponsive_h_
+
+#include "Arduino.h"
+#include "../Settings/Settings.h"
+#include "MIDI_Control_Element.h"
+#include "../ExtendedInputOutput/ExtendedInputOutput.h"
+#include <ResponsiveAnalogRead.h>
+
+class AnalogResponsive : public MIDI_Control_Element
+{
+public:
+  AnalogResponsive(pin_t analogPin, uint8_t controllerNumber, uint8_t channel); // Constructor
+  ~AnalogResponsive();                                                          // Destructor
+  void invert();                                                      // Invert the analog scale
+  void map(int (*fn)(int));                                           // Change the function pointer for analogMap to a new function. It will be applied to the raw analog input value in Analog::refresh()
+
+private:
+  void refresh(); // Read the analog input value, update the average, map it to a MIDI value, check if it changed since last time, if so, send Control Change message over MIDI
+
+  ResponsiveAnalogRead *respAnalog;
+  pin_t analogPin;
+  uint8_t controllerNumber, channel, oldVal = -1;
+  int (*analogMap)(int) = identity; // function pointer to identity function f(x) → x
+
+  static int identity(int x)
+  { // identity function f(x) → x
+    return x;
+  }
+  
+  bool invertState = false;
+};
+
+#endif // AnalogResponsive_h_
