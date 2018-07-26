8437236ddac62571b44f6a5af493850839b96c23
Pieter <pieter.pas@outlook.com>
2018-07-27 01:51:12 +0200

    Fix integer overflow filter

--- src/Settings/Settings.h
+++ src/Settings/Settings.h
@@ -1,7 +1,7 @@
 
 // #define DEBUG // Print debug messages to Serial. (Not compatible with AVR, because it uses Serial.printf)
 
-#define ANALOG_FILTER_SHIFT_FACTOR 3
+#define ANALOG_FILTER_SHIFT_FACTOR 2
 
 #define BUTTON_DEBOUNCE_TIME 25 // The debounce time for momentary push buttons
 
